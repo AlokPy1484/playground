0c5f3b68e73589db7cd449ffe79abf47e3232a19
AlokPy1484 <alokpandey1483@gmail.com>
2026-08-22 23:59:50 +0530

    ScrollParallaxCard: buggy

--- app/ScrollParallaxCard/page.tsx
+++ app/ScrollParallaxCard/page.tsx
@@ -1,16 +1,29 @@
+"use client"
+
 import ScrollParallaxCard from "./scrollParallaxCard"
 import { projects } from "./data"
+import { useRef } from "react"
+import { useScroll } from "motion/react"
 
 
 
 
 export default function page() {
 
+    const ref = useRef(null)
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['start start', 'end end']
+    })
+
+
     return (
-        <div className="flex flex-col justify-center items-center gap-8 w-screen h-full mt-80">
-            {projects.map((card, idx) => (
-                <ScrollParallaxCard color={card.color} i={idx} />
-            ))}
+        <div ref={ref} className="flex flex-col justify-center items-center gap-8 w-screen h-full mt-80">
+            {projects.map((card, idx) => {
+                const targetScale = 1 - ((projects.length) - idx * 0.05)
+                return <ScrollParallaxCard color={card.color} i={idx} range={[idx * 0.25, 1]} targetScale={targetScale} progress={scrollYProgress} />
+            })}
         </div>
     )
 }
--- app/ScrollParallaxCard/scrollParallaxCard.tsx
+++ app/ScrollParallaxCard/scrollParallaxCard.tsx
@@ -10,7 +10,7 @@ import { useRef } from "react";
 
 
 
-export default function ScrollParallaxCard({ i, color, }) {
+export default function ScrollParallaxCard({ i, color, progress, range, targetScale }) {
     const ref = useRef<HTMLDivElement>(null)
 
     const { scrollYProgress } = useScroll({
@@ -20,10 +20,16 @@ export default function ScrollParallaxCard({ i, color, }) {
 
     const scale = useTransform(scrollYProgress, [0, 1], [2, 1])
 
+    const mainScale = useTransform(progress, range, [1, targetScale])
+
     return (
         <div ref={ref} className="sticky top-0 h-screen">
-            <div className="relative flex justify-center items-center w-[1000px] h-[500px] rounded-4xl"
-                style={{ backgroundColor: color, top: `calc(10vh + ${i * 25}px)` }}>
+            <motion.div className="relative flex justify-center items-center w-[1000px] h-[500px] rounded-4xl"
+                style={{
+                    backgroundColor: color,
+                    top: `calc(10vh + ${i * 25}px)`,
+                    scale: mainScale
+                }}>
                 <div className="object-cover overflow-hidden rounded-3xl">
                     <motion.div
                         style={{ scale }}
@@ -31,7 +37,7 @@ export default function ScrollParallaxCard({ i, color, }) {
                         <Image unoptimized src={"https://placehold.co/980x480"} alt="image" width={980} height={480} />
                     </motion.div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
